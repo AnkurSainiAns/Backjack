75a8de193288fb8acf2bf70703617f45b1f71f3a
AnkurSainiAns <an2811saini@gmail.com>
2025-08-15 21:35:16 +0530

    new update

---
+++ script.js
@@ -0,0 +1,28 @@
+let card1el = document.getElementById("card1");
+let card2el = document.getElementById("card2");
+let sumel = document.getElementById("sum");
+let resultel = document.getElementById("result");
+
+function startGame() {
+    card1 = Math.floor(Math.random() * 11) + 1; 
+    card2 = Math.floor(Math.random() * 11) + 1; 
+    sum = card1 + card2;
+    card1el.innerHTML = "First Card: " + card1;
+    card2el.innerHTML = "Second Card: " + card2;
+    sumel.innerHTML = "Sum: " + sum;
+    if (sum < 21) {
+        result = `Sum Of Cards is = ${sum} You are safe 😅`;
+    } else if (sum === 21) {
+        result = `Sum Of Cards is = ${sum} "You win 🤩"`;
+    } else {
+        result = `Sum Of Cards is = ${sum} You lose! 😭`;
+    }
+    resultel.innerHTML = result;
+}
+
+function Reset() {
+    card1el.innerHTML = "First Card: ";
+    card2el.innerHTML = "Second Card: ";
+    sumel.innerHTML = "Sum: ";
+    resultel.innerHTML = "Result: ";
+}
